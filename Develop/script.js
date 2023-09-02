3490273b682a39c723e692cde67848d101d45b3b
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-09-01 23:04:34 -0500

    cloned starter code into new repo and created pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,25 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//Function called generatepassword
+function generatePassword() {
+  //prompt user for pw length
+  //verify that length entered is between 8 and 128
+  //if it isn't, keep prompting until they enter a valid pw
+  //ask user if they want to include:
+    //capital letters
+    //lowercase letters
+    //numeric
+    //special characters
+  //keep track of answers in variables
+  
+  //after each correct prmopt, validate 
+  
+  //generte password if all prompts correct
+  
+  return "";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
